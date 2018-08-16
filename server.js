415eee726dbb5f255c820c0824c29c1240331c54
Sarthak Sachdeva <sarthaksachdeva13@gmail.com>
2018-08-16 10:16:02 -0400

    Added heroku CORS

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.get('/', (req, res) =>
     res.send('Hello World'));
 
 app.use(function (req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "https://sarthakangular.herokuapp.com");
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Origin", "https://sarthakangular.herokuapp.com");
+    // res.header("Access-Control-Allow-Origin", "http://localhost:4200");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
